46840c7c0da17e4e6f50e250a3be72efd67fb0a7
Rutar Andriy <RutarAndriy@gmail.com>
2026-03-29 20:28:46 +0300

    Removed unnecessary libraries

--- src/main/java/com/rutar/ttool_dk2/Utils.java
+++ src/main/java/com/rutar/ttool_dk2/Utils.java
@@ -7,7 +7,6 @@ import java.util.*;
 import javax.swing.*;
 import java.nio.charset.*;
 import javax.swing.filechooser.*;
-import org.apache.commons.compress.compressors.deflate.*;
 
 // ............................................................................
 /// Корисні допоміжні методи
@@ -183,47 +182,6 @@ public static char fromStringToChar (String s) {
     
     if (s.length() == 1) { return s.charAt(0); }
     else { return fromCodeToCP1251Char(Integer.parseInt(s)); }  
-}
-
-// ============================================================================
-/// Розпакування даних, запакованих з допомогою алгоритму zlib (deflate)
-/// @param compressed дані, запаковані з допомогою алгоритму zlib (deflate)
-/// @return розпаковані дані
-
-public static byte[] zlibDecompress (byte[] compressed) {
-
-try (ByteArrayInputStream bais = new ByteArrayInputStream(compressed);
-     DeflateCompressorInputStream dis = new DeflateCompressorInputStream(bais);
-     ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-
-    int n;
-    byte[] byteBuffer = new byte[8192];
-    
-    while ((n = dis.read(byteBuffer)) != -1) { baos.write(byteBuffer, 0, n); }
-    
-    return baos.toByteArray();
-}
-
-catch (Exception e) { IO.println("zlib decompress error");
-                      return null; }
-}
-
-// ============================================================================
-/// Запакування даних з допомогою алгоритму zlib (deflate)
-/// @param decompressed незапаковані дані
-/// @return запаковані дані
-
-public static byte[] zlibCompress (byte[] decompressed) {
-
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-    try (DeflateCompressorOutputStream dos =
-     new DeflateCompressorOutputStream(baos)) { dos.write(decompressed); }
-
-    catch (Exception e) { IO.println("zlib compress error");
-                          return null; }
-
-    return baos.toByteArray();
 }
 
 // ============================================================================
